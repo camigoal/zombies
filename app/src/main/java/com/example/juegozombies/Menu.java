ede28c0d109b0fe9d0441a6bf737eef8029f07d0
Jonathan Reyes <jonathandavidreyesparra@gmail.com>
2022-11-14 14:59:26 -0500

    06- cerrarsesion

--- app/src/main/java/com/example/juegozombies/Menu.java
+++ app/src/main/java/com/example/juegozombies/Menu.java
@@ -2,13 +2,51 @@ package com.example.juegozombies;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class Menu extends AppCompatActivity {
-
+    FirebaseAuth auth;
+    FirebaseUser user;
+    Button CerrarSesion;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu);
+        auth = FirebaseAuth.getInstance();
+        user = auth.getCurrentUser();
+        CerrarSesion = findViewById(R.id.CerrarSesion);
+        CerrarSesion.setOnClickListener(new View.OnClickListener() {
+        @Override
+        public void onClick(View view) {
+            CerrarSesion();
+        }
+    });
+}
+    // ESTE METODO SE EJECUTA CUANDO SE ABRE EL MINIJUEGO//
+    @Override
+    protected void onStart() {
+    UsuarioLogueado();
+    super.onStart();
+}
+private void UsuarioLogueado() {
+    if (user != null) {
+        Toast.makeText(this, "Jugador en linea", Toast.LENGTH_SHORT).show();
     }
+        else{
+            startActivity(new Intent(Menu.this, MainActivity.class));
+            finish();
+        }
+    }
+    private void CerrarSesion() {
+        auth.signOut();
+        startActivity (new Intent( Menu. this, MainActivity.class));
+        Toast.makeText(this,"Cerrado sesión exitosamente", Toast.LENGTH_SHORT).show();
+       }
 }
